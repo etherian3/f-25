94465edd4881ae7816b68593a5120f4ddcd4d106
etheriannn <rianziwalker@gmail.com>
2025-03-26 19:40:01 +0700

    feat: add gas tracking for withdrawal function in FundMeTest

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,6 +11,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("USER");
     uint256 constant SEND_VALUE = 0.1 ether; // 1000000000000000000
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         // fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -77,7 +78,7 @@ contract FundMeTest is Test {
         uint256 startingFundMeBalance = address(fundme).balance;
 
         // Act
-        vm.prank(fundme.getOwner());
+        vm.prank(fundme.getOwner()); // c: 200
         fundme.withdraw();
 
         // Assert
