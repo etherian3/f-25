ecbf749aa7cd663f1b3a432511eb3e3ebca592ab
etheriannn <rianziwalker@gmail.com>
2025-03-28 19:16:17 +0700

    feat: add withdraw functionality to Interactions script for FundMe contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -8,20 +8,32 @@ import {FundMe} from "../src/FundMe.sol";
 contract FundFundMe is Script {
     uint256 constant SEND_VALUE = 0.01 ether;
 
-    function fundFundMe(address mostRecentDeployed) public {
-        vm.startBroadcast();
+    function fundFundMe(address mostRecentDeployed) public payable {
         FundMe(payable(mostRecentDeployed)).fund{value: SEND_VALUE}();
-        vm.stopBroadcast();
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
     function run() external {
-        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
-            "FundMe",
-            block.chainid
-        );
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        vm.startBroadcast();
         fundFundMe(mostRecentDeployed);
+        vm.stopBroadcast();
     }
 }
 
-contract WithdrawFundMe is Script {}
+contract WithdrawFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function withdrawFundMe(address mostRecentDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentDeployed);
+        vm.stopBroadcast();
+    }
+}
